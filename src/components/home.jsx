62836dd1f92bf82b2d310b8845d383e2ebd8a652
Tyler Tulsky <ttulsky@gmail.com>
2024-05-15 11:37:06 +0900

    title bug patch

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,12 +3,13 @@ import React from "react";
 function Home() {
   return (
     <header className="App-header">
-      <h2>
+      <h3>
         Welcome Fam
         <br />
-      </h2>
+      </h3>
       <p>Lets Get Swole!</p>
       <img src="/fitness2.png" className="App-logo" alt="logo" />
+
       <br />
     </header>
   );
